0db12004ed540ca48c2aad7538d6166d1c3f262d
cristianlarrain <clarrain@gmail.com>
2021-05-10 20:22:59 -0400

    Update ConexionDatos.cs

--- ServicioWeb.CasoLibros/ConexionDatos.cs
+++ ServicioWeb.CasoLibros/ConexionDatos.cs
@@ -4,7 +4,7 @@ namespace Datos
     public class ConexionDatos
     {
        private const string cadenaConexion = @"Data Source=DESKTOP-M1PKGM9;Initial Catalog=LibreriaVirtual;Integrated Security=True";
-       
+      
         public static string CadenaConexion
         {
             get { return cadenaConexion; }
